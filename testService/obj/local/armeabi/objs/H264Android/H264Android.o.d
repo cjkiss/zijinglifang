a4b686e0a07f526a444885a7c57dd68bcbcfbdf1
jkiss <pengzhi168@gmai.com>
2014-10-31 16:18:01 +0800

    TestService
    parse .h246

---
+++ testService/obj/local/armeabi/objs/H264Android/H264Android.o.d
@@ -0,0 +1,170 @@
+/home/zhutj/androidProject/h264_project/obj/local/armeabi/objs/H264Android/H264Android.o: \
+ /home/zhutj/androidProject/h264_project/jni/H264Android.c \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/string.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/common.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/ctype.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/errno.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/inttypes.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/math.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/attributes.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/common.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/mem.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/error.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/avutil.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/mathematics.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/rational.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/intfloat_readwrite.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/log.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/pixfmt.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavcodec/avcodec.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/samplefmt.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/avutil.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/cpu.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/dict.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavcodec/version.h \
+ /home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/audioconvert.h
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/common.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/ctype.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/inttypes.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/home/zhutj/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/math.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/attributes.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/common.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/mem.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/error.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/avutil.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/mathematics.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/rational.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/intfloat_readwrite.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/log.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/pixfmt.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavcodec/avcodec.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/samplefmt.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/avutil.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/cpu.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/dict.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavcodec/version.h:
+
+/home/zhutj/androidProject/h264_project/jni/ffmpeg/libavutil/audioconvert.h:
